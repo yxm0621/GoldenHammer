2c72b30ebd02658560d4732706f72e61c9fde4cb
Claire Yin <yxm0621@gmail.com>
2015-03-16 15:34:45 -0700

    alpha

---
+++ GetRichorDieTrying/Assets/__Scripts/LevelSegments.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSegments : MonoBehaviour {
+
+	public GameManager 				gameMain;
+
+	public GameObject				thisSegment;
+
+	//TODO create Array of potential objects to be input and their position to be created. Randomize their generation.
+
+	public Vector3					currentPos;
+
+	private float					moveToPos = -10.0f;
+	public float					timeToPos = .2f;
+
+	public bool[,]					segEmpty;
+
+	public float					roadInitialSpeed = .2f;
+	public float					sidewalkInitialSpeed = .1f;
+	public float					ComboSpeedBonus = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+		gameMain = GameManager.manager;
+
+		thisSegment = this.gameObject;
+
+		iTween.Init (thisSegment);
+
+		/*
+		if(thisSegment.name == "LevelSegments1"){
+			timeToPos /= 5;
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+		if(thisSegment.name == "LevelSegments2"){
+			timeToPos /= 4;
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+		if(thisSegment.name == "LevelSegments3"){
+			timeToPos /= 3;
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+		if(thisSegment.name == "LevelSegments4"){
+			timeToPos /= 2;
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+		if(thisSegment.name == "LevelSegments5"){
+			timeToPos /= 1;
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+		if(thisSegment.name == "LevelSegments(Clone)"){
+			Debug.Log (timeToPos + " Seconds to Position");
+		}
+
+
+
+		iTween.MoveTo(thisSegment, iTween.Hash("z", moveToPos, "easetype", "linear","time", timeToPos, "oncomplete", "EndOfRoad"));
+
+		*/
+		//thisSegment.rigidbody.AddForce(Vector3.forward); //Give forward motion
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.gameState == GameManager.State.InGame ||
+		    GameManager.manager.currentLevel == "GameOver") {
+			ComboSpeedBonus = (gameMain.bonus - 1)/8f;
+			currentPos = thisSegment.transform.position;
+
+			//		thisSegment.transform.Translate(new Vector3 (0,0, -1) * Time.deltaTime * timeToPos);
+
+			if(thisSegment.transform.position.z <= -6f){
+				//Debug.Log("Spawning new Segment & Deleting Old");
+				Destroy (thisSegment);
+				gameMain.NewSegment();
+			}
+
+			if (characterController.onSidewalk){
+				timeToPos = sidewalkInitialSpeed + ComboSpeedBonus;
+			} else {
+				timeToPos = roadInitialSpeed + ComboSpeedBonus;
+			}
+
+//		if (characterController.forward){
+			GameObject[] segs = GameObject.FindGameObjectsWithTag("Segment");
+			GameObject[] obss = GameObject.FindGameObjectsWithTag("Obstacle");
+			foreach (GameObject seg in segs) {
+//				seg.transform.position -= new Vector3(0,0,3f);
+				seg.transform.Translate(new Vector3 (0,0, -1) * Time.deltaTime * timeToPos);
+			}
+			foreach (GameObject obs in obss) {
+//				obs.transform.position -= new Vector3(0,0,3f);
+				if (obs.name.Contains("PoliceCarL") || obs.name.Contains("TankL") ||
+				    obs.name.Contains("Human_")) {
+					obs.transform.Translate(new Vector3 (0,0, 1) * Time.deltaTime * timeToPos);
+				} else if (obs.name.Contains("PoliceCarR") || obs.name.Contains("TankR")) {
+					obs.transform.Translate(new Vector3 (1,0,0) * Time.deltaTime * timeToPos);
+				} else {
+					obs.transform.Translate(new Vector3 (0,0, -1) * Time.deltaTime * timeToPos);
+				}
+				if(obs.transform.position.z <= -6f){
+					Destroy (obs);
+				}
+			}
+//				characterController.forward = false;
+//		}
+		}
+		gameMain.moveSpeed = timeToPos;
+	}
+
+	//
+//	void OnTriggerEnter(Collider other){
+//		if(other.tag == "SegmentKill"){
+//			Debug.Log ("Destroy Segment");
+//			Destroy (thisSegment);
+//			gameMain.NewSegment();
+//		}
+//	}
+
+}
---
+++ GetRichorDieTrying/Assets/__Scripts/SceneManager.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	public enum scene{city, countryside, wilderness, space, ocean, battlefield};
+
+	public scene previousScene;
+	public scene currentScene;
+	public float timePassed;
+
+	//public GameObject	mainCamera; //This script is attached to the main camera
+
+	public GameObject[] building;
+	public GameObject[] sidewalk;
+	public GameObject[] road;
+
+	public GameObject[] cityBuilding;
+	public GameObject[] citySidewalk;
+	public GameObject[] cityRoad;
+
+	public GameObject[] countrysideBuilding;
+	public GameObject[] countrysideSidewalk;
+	public GameObject[] countrysideRoad;
+
+	public GameObject[] wildernessBuilding;
+	public GameObject[] wildernessSidewalk;
+	public GameObject[] wildernessRoad;
+
+	public GameObject[] spaceBuilding;
+	public GameObject[] spaceSidewalk;
+	public GameObject[] spaceRoad;
+
+	public GameObject[] oceanBuilding;
+	public GameObject[] oceanSidewalk;
+	public GameObject[] oceanRoad;
+	
+	public GameObject[] battlefieldBuilding;
+	public GameObject[] battlefieldSidewalk;
+	public GameObject[] battlefieldRoad;
+
+	public int[,] buildingData;
+	public int[,] sidewalkData;
+
+	public bool reloadScene = false;
+	
+	public Material						citySky;
+	public Material						nightSky;
+	public Material						spaceSky;
+
+	// Use this for initialization
+	void Start () {
+		changeScene (scene.city);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timePassed = Time.timeSinceLevelLoad;
+//		if (timePassed <= 30f) {
+//			currentScene = scene.city;
+//		}
+	}
+
+	public void changeScene(scene newScene) {
+		previousScene = currentScene;
+		currentScene = newScene;
+		switch(currentScene) {
+		case scene.city:
+			loadCity();
+			break;
+		case scene.countryside:
+			loadCountryside();
+			break;
+		case scene.wilderness:
+			loadWilderness();
+			break;
+		case scene.space:
+			loadSpace();
+			break;
+		case scene.ocean:loadOcean();
+			break;
+		case scene.battlefield:
+			loadBattlefield();
+			break;
+		}
+		GameObject.Find("Grid").GetComponent<Grid> ().setGrid (building, sidewalk, road, buildingData, sidewalkData);
+
+		if (previousScene == scene.space) {
+			iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene").Play();
+			iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene1").Play();
+			iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene2").Play();
+			reloadScene = true;
+		}
+	}
+
+	void loadCity(){
+		Camera.main.GetComponent<Skybox> ().material = citySky;
+		building = cityBuilding;
+		sidewalk = citySidewalk;
+		road = cityRoad;
+		buildingData = new int[10, 4] {{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0}};
+		sidewalkData = new int[21, 4] {{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0}};
+	}
+
+	void loadCountryside(){
+		building = countrysideBuilding;
+		sidewalk = countrysideSidewalk;
+		road = countrysideRoad;
+	}
+	void loadWilderness(){
+		building = wildernessBuilding;
+		sidewalk = wildernessSidewalk;
+		road = wildernessRoad;
+		
+	}
+	void loadSpace(){
+		iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene").Play();
+		iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene1").Play();
+		iTweenEvent.GetEvent(Camera.main.gameObject, "loadScene2").Play();
+		building = spaceBuilding;
+		sidewalk = spaceSidewalk;
+		road = spaceRoad;
+		buildingData = new int[9, 4] {{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},
+			{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0}};
+		sidewalkData = new int[6, 4] {{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,1,0,0}};
+		Camera.main.GetComponent<Skybox> ().material = spaceSky;
+		reloadScene = true;
+	}
+	void loadOcean(){
+		building = oceanBuilding;
+		sidewalk = oceanSidewalk;
+		road = oceanRoad;
+		
+	}
+	void loadBattlefield(){
+		building = battlefieldBuilding;
+		sidewalk = battlefieldSidewalk;
+		road = battlefieldRoad;
+		
+	}
+}
