0df1a61735937c60390f6457f5f349d72c04b383
Claire Yin <yxm0621@gmail.com>
2015-04-07 16:13:41 -0700

    update

---
+++ GetRichorDieTrying/Assets/__Scripts/NewSegTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewSegTrigger : MonoBehaviour {
+    //generate new segment
+    void OnTriggerExit(Collider other)
+    {
+        GameManager.manager.NewSegment();
+    }
+}
---
+++ GetRichorDieTrying/Assets/__Scripts/SmashRoad.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmashRoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Smash Road!!");
+        //if (GameManager.gameState == GameManager.State.InGame)
+        //{
+        //    GameObject.Find("Hammer").GetComponent<HammerBehavior>().hammerSmash(gameObject.transform.position);
+        //    iTween.MoveFrom(gameObject, iTween.Hash("z", gameObject.transform.position.z + 0.25f, "y", gameObject.transform.position.y + -0.25f, "time", 0.5f));
+        //}
+    }
+}
