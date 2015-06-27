2b8587c9c05e1f79671919430b8879f794d5f3e8
yxm0621 <yxm0621@gmail.com>
2015-06-26 17:32:54 -0700

    fix hitobj spin problem

---
+++ GetRichorDieTrying/Assets/__Scripts/HitObj.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitObj : MonoBehaviour {
+    GameManager gameMain;
+    Vector3 objPos;
+
+	// Use this for initialization
+	void Start () {
+        gameMain = GameManager.manager;
+        objPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+        gameObject.transform.position += new Vector3(0, .2f, -.2f);
+        iTween.MoveTo(gameObject, iTween.Hash("position", objPos, "time", .5f));
+        //gameMain.objectName = gameObject.name;
+        //gameMain.coinPos = gameObject.transform.position;
+        //gameMain.CashOut(0);
+        //Destroy(gameObject);
+    }
+}
